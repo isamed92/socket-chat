e9f6ab0c68730af9c49c938574628c0a3b557835
Isa Notebook <isamed92@gmail.com>
2018-10-20 10:35:39 -0300

    primer commit: backend chat

---
+++ server/classes/usuarios.js
@@ -0,0 +1,52 @@
+
+
+
+class Usuarios{
+
+
+    constructor(){
+        this.personas = [];
+    }
+
+    agregarPersona(id, nombre, sala){
+        let persona = {
+            id,
+            nombre,
+            sala
+        }
+
+        this.personas.push(persona);
+
+        return this.personas;
+    }
+
+    getPersona(id){
+        let persona = this.personas.filter(per => per.id === id)[0];
+        return persona;
+    }
+
+    getPersonas(){
+        return this.personas;
+    }
+
+    getPersonasPorSala(sala){
+        let personasEnSala = this.personas.filter(persona =>{
+            return persona.sala ===sala;
+        })
+
+        return personasEnSala;
+
+    }
+
+    borrarPersona(id){
+        let personaBorrada = this.getPersona(id);
+        // console.log();
+        this.personas = this.personas.filter((persona) => persona.id != id);
+        return personaBorrada;
+    }
+
+}
+
+module.exports ={
+    Usuarios
+}
